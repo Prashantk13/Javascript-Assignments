bc30f091b76c3997e4367b715e77ad443f53086c
Prashantk13 <65998202+Prashantk13@users.noreply.github.com>
2020-08-02 13:59:06 +0530

    Create Project.js

---
+++ Project.js
@@ -0,0 +1,27 @@
+const clear = document.getElementById('clear-list');
+const list = document.getElementById('list');
+const myform = document.querySelector('#todo');
+function clearitem(){
+    list.innerHTML = "";
+}
+const button = document.getElementById('btn');
+// button.onclick = function(){
+function additem(){
+    let item = myform.value;
+    let text = document.createTextNode(item);
+    let listitem = document.createElement('li');
+    listitem.appendChild(text);
+    list.appendChild(listitem);
+}
+// myform.addEventListener('keyup',function(event){
+//     if(event.keycode === 13){
+//         event.preventDefault()
+//         button.onclick;
+//     }
+// });
+// button.addEventListener('keypress',function(event){
+//     if(event.keycode == 10){
+//         event.preventDefault();
+//         button.onclick();
+//     }
+// });
